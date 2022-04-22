017e952b2140bb0ea7ff8836ef11fee48e6322aa
Sarah Moss <sarah-moss@live.com>
2022-04-22 03:49:40 +0000

    Comment out code so edgecase match assignment description

--- timer1.js
+++ timer1.js
@@ -13,16 +13,11 @@ for (let value in values) {
     }, (values[value]) * 1000);
   
   } else {
-    process.stdout.write(`🔕 You entered '${values[value]}' Please enter a valid number 🔕 \n`)
+    return; 
+    // process.stdout.write(`🔕 You entered '${values[value]}' Please enter a valid number 🔕 \n`)
   }
 
 };
-
-// EDGE CASES
-
-// No numbers are provided: Easy. It should just not beep at all and end immediately since no beeps should get scheduled.
-// An input is a negative number: Ignore/skip any numbers that are negative. We can't schedule anything in the past.
-// An input is not a number: Ignore/skip these as well, instead of attempting to call setTimeout with a non-number.
 
 // DEVELOPMENT NOTES TO TEST
 
